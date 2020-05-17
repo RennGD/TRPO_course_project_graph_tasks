f641717e3560c7afeefb0b3b0c259717d13a0c63
STAS <renn.dima1245@gmail.com>
2020-05-17 16:08:26 +0700

    Fix one_path.h

--- src/one_path.h
+++ src/one_path.h
@@ -1,14 +1,16 @@
 #ifndef ONE_PATH
 #define ONE_PATH
 
-#include <vector>
-#include <iostream>
-#include <iomanip>
 #include <climits>
+#include <iomanip>
+#include <iostream>
+#include <vector>
 
-using namespase std;
+using namespace std;
 
-vector<int>* DijkstraMin(int Cities, int MainPeak, vector<vector<int>> TableLength);
-vector<int>* DijkstraMax(int Cities, int MainPeak, vector<vector<int>> TableLength);
+vector<int>*
+DijkstraMin(int Cities, int MainPeak, vector<vector<int>> TableLength);
+vector<int>*
+DijkstraMax(int Cities, int MainPeak, vector<vector<int>> TableLength);
 
 #endif

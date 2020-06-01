8a124bd11c07689e75c9df737d884af70f2717cd
LidiaKorolek <koroleva-lida@bk.ru>
2020-06-02 03:24:03 +0800

    Creating a test for dijkstra_max

---
+++ test/test_dijkstra_max.cpp
@@ -0,0 +1,39 @@
+#include "catch.hpp"
+#include "one_path.h"
+
+TEST_CASE("Check dijkstra max for correctness", "[dijkstra_max]")
+{
+    std::vector<vector<int>> v;
+    std::vector<int> max_dist;
+
+    v.resize(2, vector<int>(2));
+    max_dist.resize(2);
+    v[0][0] = v[1][1] = max_dist[0] = 0;
+    v[1][0] = v[0][1] = max_dist[1] = 5;
+    max_dist[0] = 10;
+
+    REQUIRE(dijkstra_max(2, 0, v) == max_dist);
+
+    v[1][0] = v[0][1] = max_dist[0] = 9;
+    max_dist[1] = 18;
+
+    REQUIRE(dijkstra_max(2, 1, v) == max_dist);
+
+    v.resize(3, vector<int>(3));
+    min_dist.resize(3);
+    v[2][2] = 0;
+    v[0][1] = v[1][0] = max_dist[1] = 3;
+    v[0][2] = v[2][0] = 0;
+    v[1][2] = v[2][1] = max_dist[0] = 6;
+    max_dist[2] = 9;
+
+    REQUIRE(dijkstra_max(3, 0, v) == max_dist);
+
+    v[0][2] = v[2][0] = 6;
+    v[1][2] = v[2][1] = 0;
+    max_dist[0] = 3;
+    max_dist[1] = 6;
+    max_dist[2] = 9;
+
+    REQUIRE(dijkstra_max(3, 1, v) == max_dist);
+}

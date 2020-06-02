b2dc91adcf4847f7213803b50a110a68d811081a
LidiaKorolek <koroleva-lida@bk.ru>
2020-06-02 20:18:49 +0800

    Fix test max

--- test/test_dijkstra_max.cpp
+++ test/test_dijkstra_max.cpp
@@ -4,6 +4,7 @@
 TEST_CASE("Check dijkstra max for correctness", "[dijkstra_max]")
 {
     std::vector<vector<int>> v;
+    std::vector<vector<int>> ve;
     std::vector<int> max_dist;
 
     v.resize(2, vector<int>(2));
@@ -19,15 +20,14 @@ TEST_CASE("Check dijkstra max for correctness", "[dijkstra_max]")
 
     REQUIRE(dijkstra_max(2, 1, v) == max_dist);
 
-    v.resize(3, vector<int>(3));
+    ve.resize(3, vector<int>(3));
     max_dist.resize(3);
-    v[2][2] = 0;
-    v[0][1] = v[1][0] = max_dist[1] = 3;
-    v[0][2] = v[2][0] = 0;
-    v[1][2] = v[2][1] = max_dist[0] = 6;
+    ve[0][1] = ve[1][0] = max_dist[1] = 3;
+    ve[0][2] = ve[2][0] = ve[2][2] = 0;
+    ve[1][2] = ve[2][1] = max_dist[0] = 6;
     max_dist[2] = 9;
 
-    REQUIRE(dijkstra_max(3, 0, v) == max_dist);
+    REQUIRE(dijkstra_max(3, 0, ve) == max_dist);
 
     v[0][2] = v[2][0] = 6;
     v[1][2] = v[2][1] = 0;
@@ -35,5 +35,5 @@ TEST_CASE("Check dijkstra max for correctness", "[dijkstra_max]")
     max_dist[1] = 6;
     max_dist[2] = 9;
 
-    REQUIRE(dijkstra_max(3, 1, v) == max_dist);
+    REQUIRE(dijkstra_max(3, 1, ve) == max_dist);
 }

7e4d3352edc5efc5b55d4ba68ca447d92c07be2e
LidiaKorolek <koroleva-lida@bk.ru>
2020-06-02 19:51:21 +0800

    Fix test X2

--- test/test_dijkstra_min.cpp
+++ test/test_dijkstra_min.cpp
@@ -3,10 +3,9 @@
 
 TEST_CASE("Check dijkstra min for correctness", "[dijkstra_min]")
 {
-    std::vector<vector<int>> v;
+    std::vector<vector<int>> v(2, vector<int>(2));
     std::vector<int> min_dist;
 
-    v.resize(2, vector<int>(2));
     min_dist.resize(2);
     v[0][0] = v[1][1] = min_dist[0] = 0;
     v[1][0] = v[0][1] = min_dist[1] = 5;
@@ -18,20 +17,20 @@ TEST_CASE("Check dijkstra min for correctness", "[dijkstra_min]")
 
     REQUIRE(dijkstra_min(2, 1, v) == min_dist);
 
-    v.resize(3, vector<int>(3));
-    min_dist.resize(3,0);
-    v[2][2] = v[0][0] = v[1][1] = min_dist[0] = 0;
-    v[0][1] = v[1][0] = min_dist[1] = 3;
-    v[0][2] = v[2][0] = 8;
-    v[1][2] = v[2][1] = 4;
+    std::vector<vector<int>> ve(3, vector<int>(3));
+    min_dist.resize(3, 0);
+    ve[2][2] = ve[0][0] = ve[1][1] = min_dist[0] = 0;
+    ve[0][1] = ve[1][0] = min_dist[1] = 3;
+    ve[0][2] = ve[2][0] = 8;
+    ve[1][2] = ve[2][1] = 4;
     min_dist[2] = 7;
 
-    REQUIRE(dijkstra_min(3, 0, v) == min_dist);
+    REQUIRE(dijkstra_min(3, 0, ve) == min_dist);
 
-    v[0][1] = v[1][0] = 9;
+    ve[0][1] = ve[1][0] = 9;
     min_dist[0] = 8;
     min_dist[1] = 4;
     min_dist[2] = 0;
 
-    REQUIRE(dijkstra_min(3, 3, v) == min_dist);
+    REQUIRE(dijkstra_min(3, 3, ve) == min_dist);
 }

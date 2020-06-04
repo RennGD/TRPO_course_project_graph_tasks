b2f7aaaa0917376796d929680138d39e9b94d0d9
LidiaKorolek <koroleva-lida@bk.ru>
2020-06-05 02:02:19 +0800

    Fix connectivity in h

--- src/check_graph.h
+++ src/check_graph.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool connectivity(vector<vector<int>> &table_length, int cities, int main_peak);
+bool connectivity(vector<vector<int>>& table_length, int cities, int main_peak);
 bool check_loop(vector<vector<int>> table_length);
 
 #endif

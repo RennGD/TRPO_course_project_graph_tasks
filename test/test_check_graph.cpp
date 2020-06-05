db814f8b4658610f46697bb235167829e521b978
STAS <renn.dima1245@gmail.com>
2020-06-05 10:24:44 +0700

    Add tests for check_loop

--- test/test_check_graph.cpp
+++ test/test_check_graph.cpp
@@ -1,10 +1,12 @@
 #include "catch.hpp"
 #include "check_graph.h"
 
+using namespace std;
+
 TEST_CASE("Check connectivity for correctness", "[connectivity]")
 {
-    std::vector<vector<int>> v;
-    std::vector<vector<int>> ve;
+    vector<vector<int>> v;
+    vector<vector<int>> ve;
 
     v.resize(2, vector<int>(2));
     v[0][0] = v[1][1] = v[1][0] = v[0][1] = 0;
@@ -25,4 +27,24 @@ TEST_CASE("Check connectivity for correctness", "[connectivity]")
     ve[1][2] = ve[2][1] = 6;
 
     REQUIRE(connectivity(ve, 0) == true);
+}
+
+TEST_CASE("Test check_loop", "[check_loop]")
+{
+    vector<vector<int>> v;
+
+    v = {{0}};
+    REQUIRE(check_loop(v) == false);
+
+    v = {{1}};
+    REQUIRE(check_loop(v) == true);
+
+    v = {{0, 1}, {1, 0}};
+    REQUIRE(check_loop(v) == false);
+
+    v = {{1, 1}, {1, 0}};
+    REQUIRE(check_loop(v) == true);
+
+    v = {{1, 1}, {1, 1}};
+    REQUIRE(check_loop(v) == true);
 }
